739325b2fda2c988332aecb3744cabb24f38394b
Valer100 <122545522+Valer100@users.noreply.github.com>
2025-07-05 15:52:52 +0300

    Align tooltips accordingly

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const titlebar = document.querySelector(".titlebar");
 const backToTopButton = document.querySelector(".back-to-top");
 
+
 let scrollingToTop = false;
 let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
 
@@ -16,8 +17,6 @@ backToTopButton.onclick = scrollToTop;
 window.addEventListener("scroll", function () {
     const currentScroll = window.scrollY || document.documentElement.scrollTop;
 
-    console.log(scrollingToTop);
-
     if (currentScroll == 0) scrollingToTop = false;
 
     if (!scrollingToTop) {
@@ -30,3 +29,32 @@ window.addEventListener("scroll", function () {
 
     lastScrollTop = currentScroll;
 }, false);
+
+
+const titlebarButtons = document.querySelectorAll("a.btn-titlebar");
+
+function isElementFullyVisibleX(element) {
+    const rect = element.getBoundingClientRect();
+    return (rect.left + 1 >= 0 && rect.right + 1 <= (document.documentElement.clientWidth));
+}
+
+function adjustTooltipPositions() {
+    for (let i = 0; i < titlebarButtons.length; i++) {
+        const buttonTooltip = titlebarButtons[i].querySelector(".tooltip");
+        buttonTooltip.style.transform = "translateX(" + (33 - buttonTooltip.offsetWidth) / 2 + "px) translateY(-10px)";
+        
+        if (!isElementFullyVisibleX(buttonTooltip)) {
+            buttonTooltip.style.transform = "translateX(" + (33 - buttonTooltip.offsetWidth + 8) + "px) translateY(-10px)";
+        }
+    }
+
+    const backToTopButtonTooltip = backToTopButton.querySelector(".tooltip");
+    backToTopButtonTooltip.style.transform = "translateX(0px)";
+
+    if (!isElementFullyVisibleX(backToTopButtonTooltip)) {
+        backToTopButtonTooltip.style.transform = "translateX(" + (backToTopButton.offsetWidth - backToTopButtonTooltip.offsetWidth + 8) / 2 + "px)";
+    }
+}
+
+window.addEventListener("resize", adjustTooltipPositions, false);
+adjustTooltipPositions();
